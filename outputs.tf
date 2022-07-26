7be5fcec67a85b9c8d8b242d9a1f56ab1a09f585
adminph-de <patrick.hayo@email.de>
2022-07-26 09:11:26 +0200

    fixed

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 output "object" {
   depends_on  = [azurerm_bastion_host.this]
-  value       = azurerm_bastion_host.bastion_host
+  value       = azurerm_bastion_host.this
   description = "Contains the bastion host resource"
 }
 
 output "name" {
   depends_on  = [azurerm_bastion_host.this]
-  value       = azurerm_bastion_host.bastion_host.*.name
+  value       = azurerm_bastion_host.this.*.name
   description = "Specifies the name of the bastion host"
 }
 
 output "id" {
   depends_on  = [azurerm_bastion_host.this]
-  value       = azurerm_bastion_host.bastion_host.*.id
+  value       = azurerm_bastion_host.this.*.id
   description = "Specifies the resource id of the bastion host"
 }
 
 output "fqdn" {
   depends_on  = [azurerm_bastion_host.this]
-  value       = azurerm_bastion_host.bastion_host.dns_name
+  value       = azurerm_bastion_host.this.dns_name
   description = "The FQDN for the Bastion Host"
 }
